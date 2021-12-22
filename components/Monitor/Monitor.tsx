abd86f214de85fda38bce7d3b0da2d4b9fc29fb9
Kevin Hill <kevinkhill@gmail.com>
2021-12-22 04:41:58 -0800

    fixing some css for the monitor frame. Moving handler for softKeys up

--- components/Monitor/Monitor.tsx
+++ components/Monitor/Monitor.tsx
@@ -1,16 +1,21 @@
 import React from "react";
 
 import TitleBar from "./TitleBar";
-import SoftKey from "./SoftKey";
+// eslint-disable-next-line import/no-named-default
+import { default as Key } from "./SoftKey";
 
 interface MonitorProps {
   content: JSX.Element;
+  onSoftKey: (key: string) => void;
 }
 
-export default function Monitor({ content }: MonitorProps) {
+export default function Monitor({ content, onSoftKey }: MonitorProps) {
+  // This could be removed if nothing needs to be added from monitor handler
   const handleSoftKey = (key: string) => {
-    console.log("softkey:", key);
+    onSoftKey(key);
   };
+
+  const SoftKey = ({ value }) => <Key onClick={handleSoftKey} value={value} />;
 
   return (
     <div className="px-5 py-2 panel-frame">
@@ -20,48 +25,48 @@ export default function Monitor({ content }: MonitorProps) {
         </div>
         <div className="flex flex-row gap-1">
           <div className="flex-grow ml-5 bg-black screen-border">
-            <div className="flex flex-grow-0 h-auto overflow-y-auto text-white">
+            <div className="flex flex-grow-0 h-full overflow-y-auto text-white">
               {content}
             </div>
           </div>
           <div className="flex flex-col gap-3 py-4 px-1.5 well-border">
             <div className="flex flex-col gap-1">
-              <SoftKey onClick={handleSoftKey} value="a1" />
-              <SoftKey onClick={handleSoftKey} value="a2" />
-              <SoftKey onClick={handleSoftKey} value="a3" />
-              <SoftKey onClick={handleSoftKey} value="a4" />
+              <SoftKey value="a1" />
+              <SoftKey value="a2" />
+              <SoftKey value="a3" />
+              <SoftKey value="a4" />
             </div>
             <div className="flex flex-col gap-1">
-              <SoftKey onClick={handleSoftKey} value="a5" />
-              <SoftKey onClick={handleSoftKey} value="a6" />
-              <SoftKey onClick={handleSoftKey} value="a7" />
-              <SoftKey onClick={handleSoftKey} value="a8" />
+              <SoftKey value="a5" />
+              <SoftKey value="a6" />
+              <SoftKey value="a7" />
+              <SoftKey value="a8" />
             </div>
             <div>
-              <SoftKey onClick={handleSoftKey} value="a9" />
+              <SoftKey value="a9" />
             </div>
           </div>
         </div>
         <div className="flex flex-row gap-3 px-4 py-1.5 well-border">
           <div>
-            <SoftKey onClick={handleSoftKey} value="<" />
+            <SoftKey value="<" />
           </div>
           <div className="flex gap-1">
-            <SoftKey onClick={handleSoftKey} value="m1" />
-            <SoftKey onClick={handleSoftKey} value="m2" />
-            <SoftKey onClick={handleSoftKey} value="m3" />
-            <SoftKey onClick={handleSoftKey} value="m4" />
-            <SoftKey onClick={handleSoftKey} value="m5" />
+            <SoftKey value="m1" />
+            <SoftKey value="m2" />
+            <SoftKey value="m3" />
+            <SoftKey value="m4" />
+            <SoftKey value="m5" />
           </div>
           <div className="flex gap-1">
-            <SoftKey onClick={handleSoftKey} value="m6" />
-            <SoftKey onClick={handleSoftKey} value="m7" />
-            <SoftKey onClick={handleSoftKey} value="m8" />
-            <SoftKey onClick={handleSoftKey} value="m9" />
-            <SoftKey onClick={handleSoftKey} value="m10" />
+            <SoftKey value="m6" />
+            <SoftKey value="m7" />
+            <SoftKey value="m8" />
+            <SoftKey value="m9" />
+            <SoftKey value="m10" />
           </div>
           <div>
-            <SoftKey onClick={handleSoftKey} value=">" />
+            <SoftKey value=">" />
           </div>
         </div>
       </div>
